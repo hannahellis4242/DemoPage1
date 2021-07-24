ecea7785422c0cebc206533bf311e7dd99c32612
hannahellis4242 <hannahellis4242@gmail.com>
2021-07-24 11:24:43 +0100

    making build script also build database directories

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+dir=`pwd`;
+echo "setting up database directories"
+cd ..
+mkdir data
+cd data
+mkdir log
+cd log
+touch log.txt
+cd $dir
 echo "building client"
 cd client
 npm install
 npx webpack
+cd $dir
 echo "building server"
-cd ../server
+cd server
 npm install
 tsc
 echo "building finished"
